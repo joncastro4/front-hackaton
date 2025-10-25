25f54d415093e3a4580183f0e4443a2cd9dd2498
Jonathan Castro <uaoaar777@gmail.com>
2025-10-25 00:04:10 -0600

    dark mode desactivado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,11 +17,14 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura
+            preset: Aura,
+            options: {
+                darkModeSelector: '.my-app-dark'
+            }
         }
     }),
     provideHttpClient(),
     CookieService,
-    MessageService
+    MessageService,
   ]
 };
