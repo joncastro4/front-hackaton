493ae291296f37f10e32f97f3a5cdacd71bc218a
Jonathan Castro <uaoaar777@gmail.com>
2025-10-25 06:30:23 -0600

    aaaaaksdkja

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { Registro } from './components/registro/registro';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Inicio } from './components/dashboard/inicio/inicio';
 import { Compras } from './components/dashboard/compras/compras';
+import { CrearCuenta } from './components/crear-cuenta/crear-cuenta';
 
 export const routes: Routes = [
     { path: "", component: Landing },
     { path: "iniciar-sesion", component: InicioSesion },
     { path: "registro", component: Registro },
+    { path: "crear-cuenta", component: CrearCuenta },
     { path: "dashboard", component: Dashboard, children: [
         { path: "inicio", component: Inicio },
         { path: "compras", component: Compras }
---
+++ src/app/components/crear-cuenta/crear-cuenta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-cuenta',
+  imports: [],
+  templateUrl: './crear-cuenta.html',
+  styleUrl: './crear-cuenta.css',
+})
+export class CrearCuenta {
+
+}
